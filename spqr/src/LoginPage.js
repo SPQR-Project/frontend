cbedffa6e9f9d77f6a0151cd72fd4022f71425fc
Sean Park <seanpark98@gmail.com>
2023-05-25 23:26:35 +0900

    applied font changes and style edits

--- spqr/src/LoginPage.js
+++ spqr/src/LoginPage.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import logo from './ruun.png'; 
+import Button from '@mui/material/Button'; 
 import jwt_decode from 'jwt-decode';
 import { CognitoUser, AuthenticationDetails, CognitoUserPool } from 'amazon-cognito-identity-js';
 import { useNavigate } from 'react-router-dom';
+
+const logo = '/Assets/Images/qr_logo.png';
 
 // Styled components
 const StyledDiv = styled('div')(({ theme }) => ({
@@ -21,25 +22,56 @@ const StyledDiv = styled('div')(({ theme }) => ({
   justifyContent: 'center',
 }));
 
+const FieldWrapper = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'start', 
+  width: '352px', 
+}));
+
 const LogoImage = styled('img')(({ theme }) => ({
-  width: '200px',
-  height: '200px',
-  marginBottom: '24px',
+  width: '352px',
+  height: '352px',
+  marginBottom: '16px',
+}));
+
+const InputLabel = styled('p')(({ theme }) => ({
+  color: '#AAB0BB',
+  fontFamily: 'Pretendard-Regular',
+  fontSize: '14px',
+  marginBottom: '4px', 
 }));
 
 const InputField = styled('input')(({ theme }) => ({
-  border: '1px solid rgba(86, 92, 103, 1)',
-  borderRadius: '8px',
-  width: '55%', // reduced the width to 80%
-  height: '40px',
-  marginBottom: '16px',
-  padding: '8px',
+  border: '1px solid rgb(193,197,205)',
+  borderRadius: '4px',
+  width: '318px', 
+  height: '22px',
+  marginBottom: '8x',
+  padding: '8px 16px 8px 16px',
+  fontFamily: 'Pretendard-Regular',
   fontSize: '16px',
+  '&&:focus': {
+    outline: 'none', 
+    borderColor: '#001B27',
+  },
 }));
 
 const StyledButton = styled(Button)(({ theme }) => ({
-  width: '40%', // reduced the width to 80%
-  marginTop: '16px',
+  width: '352px', 
+  marginTop: '26px',
+  height: '40px', 
+  color: '#ffffff',
+  fontFamily: 'Pretendard-Bold', 
+  fontSize: '16px',
+  backgroundColor: '#003A54', 
+  '&&:hover': {
+    backgroundColor: '#002A3D', 
+  },
+  '&&:focus': {
+    outline: 'none', 
+    borderColor: '#001B27',
+  },
 }));
 
 function LoginPage(props) {
@@ -88,20 +120,24 @@ function LoginPage(props) {
   return (
     <StyledDiv>
       <LogoImage src={logo} alt="Logo" /> {/* Using the imported logo image */}
-      <InputField 
-        type="text" 
-        placeholder="Username" 
-        value={username} 
-        onChange={(e) => setUsername(e.target.value)} 
-      />
-      <InputField 
-        type="password" 
-        placeholder="Password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-      />
+      <FieldWrapper>
+        <InputLabel>ID</InputLabel>
+        <InputField 
+          type="text" 
+          placeholder="" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+        />
+        <InputLabel>비밀번호</InputLabel>
+        <InputField 
+          type="password" 
+          placeholder="" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+      </FieldWrapper>
       <StyledButton variant="contained" color="primary" onClick={handleLogin}>
-        Login
+        로그인
       </StyledButton>
     </StyledDiv>
   );
