9e8463103d74962b429eafb2c06118f8b9324e01
seanpark98 <seanpark98@gmail.com>
2023-06-11 22:31:15 +0900

    finished json get and population of past orders

--- spqr/src/Pages/Mobile/CartPage.js
+++ spqr/src/Pages/Mobile/CartPage.js
@@ -9,7 +9,9 @@ import rejectedIcon from "../../Assets/Images/rejected.svg";
 
 const CartPage = () => {
   const navigate = useNavigate();
-  const [PastOrdersData, setPastOrdersData] = useState(null);
+  const [pastOrdersData, setPastOrdersData] = useState(null);
+  const [pastOrdersTotalPrice, setPastOrdersTotalPrice] = useState(0);
+
   const {
     restaurant_id: restaurantId,
     branch_id: branchId,
@@ -27,9 +29,39 @@ const CartPage = () => {
           `http://localhost:8080/cart_m/${restaurantId}/${branchId}/${tableNumber}`
         );
         const jsonData = await response.json();
-        const flattenedPastOrders = jsonData.data["past_orders"].flat();
-
+        const flattenedPastOrders = jsonData.data["past_orders"].flatMap(
+          (subOrderArray) =>
+            subOrderArray.map((subOrder) => {
+              // map each main_menu to include order_status from its parent sub_order
+              subOrder.main_menus = subOrder.main_menus.map((menu) => {
+                return { ...menu, order_status: subOrder.order_status };
+              });
+              return subOrder;
+            })
+        );
         setPastOrdersData(flattenedPastOrders);
+
+        // Calculate the total price after fetching the data
+        const totalPrice = flattenedPastOrders.reduce((acc, order) => {
+          return (
+            acc +
+            order.main_menus.reduce((menuAcc, menu) => {
+              return (
+                menuAcc +
+                menu.price +
+                (menu.option_menus
+                  ? menu.option_menus.reduce(
+                      (optionAcc, option) => optionAcc + option.price,
+                      0
+                    )
+                  : 0)
+              );
+            }, 0)
+          );
+        }, 0);
+
+        // Update the total price state
+        setPastOrdersTotalPrice(totalPrice);
       } catch (error) {
         console.log("Error fetching menu data:", error);
       }
@@ -38,7 +70,7 @@ const CartPage = () => {
     fetchPastOrdersData();
   }, [restaurantId, branchId, tableNumber]);
 
-  if (!PastOrdersData) {
+  if (!pastOrdersData) {
     return <div>Loading...</div>;
   }
 
@@ -204,65 +236,95 @@ const CartPage = () => {
             <div className={styles.titlearea}>
               <div className={styles.titlearea1}>
                 <b className={styles.title}>지난 주문서</b>
-                <b className={styles.price11}>총 10,000원</b>
+                <b className={styles.price11}>
+                  총 {pastOrdersTotalPrice.toLocaleString()}원
+                </b>
               </div>
               <div className={styles.divider} />
             </div>
             <div className={styles.div13}>
-              {PastOrdersData.map((order) => (
-                <div key={order.sub_order_id} className={styles.order}>
-                  <div className={styles.div14}>
-                    <b className={styles.name2}>{order.main_menus[0].name}</b>
-                    <div className={styles.label10}>
-                      {order.order_status === "Accepted" && (
-                        <img
-                          className={styles.checkCircleIcon}
-                          alt=""
-                          src={acceptedIcon}
-                        />
-                      )}
-                      {order.order_status === "Rejected" && (
-                        <img
-                          className={styles.alertTriangleIcon}
-                          alt=""
-                          src={rejectedIcon}
-                        />
-                      )}
-                      {order.order_status === "Pending" && (
-                        <img
-                          className={styles.alertTriangleIcon}
-                          alt=""
-                          src={pendingIcon}
-                        />
-                      )}
-                      <b className={styles.state}>{order.order_status}</b>
+              {pastOrdersData.map((order) =>
+                order.main_menus.map((menu, index) => (
+                  <div
+                    key={`${order.sub_order_id}-${index}`}
+                    className={styles.order}
+                  >
+                    <div className={styles.div14}>
+                      <b className={styles.name2}>{menu.name}</b>
+                      <div
+                        className={
+                          order.order_status === "Pending"
+                            ? styles.label16
+                            : order.order_status === "Accepted"
+                            ? styles.label10
+                            : order.order_status === "Rejected"
+                            ? styles.label13
+                            : ""
+                        }
+                      >
+                        {order.order_status === "Accepted" && (
+                          <img
+                            className={styles.checkCircleIcon}
+                            alt=""
+                            src={acceptedIcon}
+                          />
+                        )}
+                        {order.order_status === "Rejected" && (
+                          <img
+                            className={styles.alertTriangleIcon}
+                            alt=""
+                            src={rejectedIcon}
+                          />
+                        )}
+                        {order.order_status === "Pending" && (
+                          <img
+                            className={styles.alertTriangleIcon}
+                            alt=""
+                            src={pendingIcon}
+                          />
+                        )}
+                        <b className={styles.state}>
+                          {order.order_status === "Pending"
+                            ? "확인 중"
+                            : order.order_status === "Accepted"
+                            ? "확인 완료"
+                            : order.order_status === "Rejected"
+                            ? "거절 됨"
+                            : ""}
+                        </b>
+                      </div>
                     </div>
-                  </div>
-                  <div className={styles.price12}>
-                    <div className={styles.div4}>
-                      {order.main_menus.map((menu) => (
+                    <div className={styles.price12}>
+                      <div className={styles.div4}>
                         <div key={menu.name} className={styles.price3}>
                           <div className={styles.priceChild5} />
                           <div className={styles.label}>
-                            가격: {menu.price}원
+                            가격 : {menu.price.toLocaleString()}원
                           </div>
                         </div>
-                      ))}
-                      {order.main_menus[0].option_menus.map((option) => (
-                        <div key={option.name} className={styles.price3}>
-                          <div className={styles.priceChild5} />
-                          <div
-                            className={styles.label}
-                          >{`${option.name} : ${option.price}원`}</div>
-                        </div>
-                      ))}
+                        {menu.option_menus.map((option) => (
+                          <div key={option.name} className={styles.price3}>
+                            <div className={styles.priceChild5} />
+                            <div className={styles.label}>{`${
+                              option.name
+                            } : ${option.price.toLocaleString()}원`}</div>
+                          </div>
+                        ))}
+                      </div>
+                      <b className={styles.totalprice2}>
+                        {(
+                          menu.price +
+                          menu.option_menus.reduce(
+                            (total, option) => total + option.price,
+                            0
+                          )
+                        ).toLocaleString()}
+                        원
+                      </b>
                     </div>
-                    <b className={styles.totalprice2}>
-                      {order.main_menus[0].price}원
-                    </b>
                   </div>
-                </div>
-              ))}
+                ))
+              )}
             </div>
           </div>
         </div>
@@ -270,11 +332,11 @@ const CartPage = () => {
       <div className={styles.div20}>
         <div className={styles.price21}>
           <div className={styles.label}>
-            <span>{`현재 주문 금액 : `}</span>
+            <span>현재 주문 금액 : </span>
             <b>20,000원</b>
           </div>
           <div className={styles.label20}>
-            <span>{`총 주문 금액 : `}</span>
+            <span>총 주문 금액 : </span>
             <b>120,000원</b>
           </div>
         </div>
